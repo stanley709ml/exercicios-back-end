0f67b4b658118d62f215fa4ce9f64bd6380a8915
Stanley <85762462+stanley709ml@users.noreply.github.com>
2026-02-12 13:43:34 -0300

    Added verif_triangulo.c

---
+++ verif_triangulo.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(){
+
+    float side1, side2, side3;
+    float equal;
+
+    printf("What's the size of the first side? ");
+    scanf("%f", &side1);
+
+    printf("What's the size of the second side? ");
+    scanf("%f", &side2);
+
+    printf("What's the size of the third side? ");
+    scanf("%f", &side3);
+
+    equal = (side1 + side2 + side3) - (side1 * 3);
+
+    if(side1 + side2 > side3 && side2 + side1 > side3 && side3 + side2 > side1 && equal == 0){
+        printf("These three sides can form a triangle.\n");
+    }else{
+        printf("These three sides cannot form a triangle.\n");
+    }
+
+    return 0;
+}
