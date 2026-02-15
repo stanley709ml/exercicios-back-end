f11f704f97ddea05fde11a9a6ca1b2e1356ed55c
Stanley <85762462+stanley709ml@users.noreply.github.com>
2026-02-14 21:48:26 -0300

    Added verif_pessoa4.c (is incomplete)

---
+++ verif_pessoa4.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+int main(){
+    
+    char name[50] = "N/A";
+    char oldest_person[50] = "N/A";
+    char youngest_woman[50] = "N/A";
+    char gender0 = '\0'
+    char gender = '\0';
+    char continue_stop0 = '\0';
+    char continue_stop = '\0';
+    int age, total_people, menOver30, womenUnder18;
+    float medium, sum_age;
+    
+    
+    
+    return 0;
+}
